8fb13cae23d442031b0a9caf35b84e491f6d6e1f
sceptre520 <denysshowdev@gmail.com>
2021-08-18 02:30:14 +0300

    first commit

---
+++ jimmy.js
@@ -0,0 +1,16 @@
+const cheerio = require('cheerio');
+const got = require('got');
+
+const vgmUrl= 'https://www.jimmybet.com.au/Sport/Australian_Rules/AFL/Matches';
+// https://www.jimmybet.com.au/Sport/Baseball/Major_League_Baseball/Matches
+
+(async () => {
+    const response = await got(vgmUrl);
+    const $ = cheerio.load(response.body);
+
+    var events = $('.framePanel')
+    // for (x in events) {
+    //     console.log(events[x])
+    // }
+    console.log(events[0])
+})()
