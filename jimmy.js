1b183fb7d8be24a71d67ceb08c9d5b078f99001e
sceptre520 <denysshowdev@gmail.com>
2021-08-18 04:29:13 +0300

    2

--- jimmy.js
+++ jimmy.js
@@ -10,22 +10,35 @@ const vgmUrl= 'https://www.jimmybet.com.au/Sport/Australian_Rules/AFL/Matches';
 
     var events = $('.framePanel')
     var len = events.length
+    var markets = []
     for (var x=0; x<len; x++) {
         var thead = $(events[x]).find('thead')
         var tds = $(thead).find('td')
         var market_len = tds.length
-        var markets = []
+        var indexes = []
         for(var y=0; y<market_len; y++) {
-            markets.push($(tds[y]).text())
+            var market_name = $(tds[y]).text()
+            var tmp_len = markets.length
+            var flag = -1
+            for(var z=0; z<tmp_len; z++) {
+                if(markets[z].key == market_name) {
+                    indexes.push(z)
+                    flag = 1
+                    break
+                }
+            }
+            if(flag == -1) {
+                markets.push({
+                    key: market_name,
+                    outcomes: []
+                })
+                indexes.push(markets.length-1)
+            }
         }
-        console.log(markets)
         var tbody = $(events[x]).find('tbody')
         var outcomes = $(tbody).children('tr')
         var out_len = outcomes.length
         for(var y=0; y<out_len; y++) {
-            var tmp = {
-                team_name: $(outcomes[y]).children('th').children('div').last().text()
-            }
             var odds = $(outcomes[y]).children('td')
             for(var z=0; z<market_len; z++) {
                 var tmp_str = $(odds[z]).children('a').text()
@@ -33,9 +46,14 @@ const vgmUrl= 'https://www.jimmybet.com.au/Sport/Australian_Rules/AFL/Matches';
                 tmp_str = tmp_arr[tmp_arr.length-1]
                 tmp_str = tmp_str.trim()
                 tmp[markets[z]] = tmp_str
+
+                markets[indexes[z]].outcomes.push({
+                    name: $(outcomes[y]).children('th').children('div').last().text(),
+                    price: tmp_str
+                })
             }
-            console.log(tmp)
         }
         console.log('------------   tbody   ---------------')
     }
+    console.log(markets)
 })()
