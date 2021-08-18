bac15097f82c270c4f9704e9bbd2493c58703392
sceptre520 <denysshowdev@gmail.com>
2021-08-18 03:52:29 +0300

    2

--- jimmy.js
+++ jimmy.js
@@ -11,7 +11,27 @@ const vgmUrl= 'https://www.jimmybet.com.au/Sport/Australian_Rules/AFL/Matches';
     var events = $('.framePanel')
     var len = events.length
     for (var x=0; x<len; x++) {
+        var thead = $(events[x]).find('thead')
+        var tds = $(thead).find('td')
+        var market_len = tds.length
+        var markets = []
+        for(var y=0; y<market_len; y++) {
+            markets.push($(tds)[y].text())
+        }
+        console.log(markets)
         var tbody = $(events[x]).find('tbody')
-        console.log(tbody)
+        var outcomes = $(tbody).children('tr')
+        var out_len = outcomes.length
+        for(var y=0; y<out_len; y++) {
+            var tmp = {
+                team_name: $(outcomes[y]).children('th').children('div').last().text()
+            }
+            var odds = $(outcomes[y]).children('td')
+            for(var z=0; z<market_len; z++) {
+                tmp[markets[z]] = $(odds[z]).children('a').text()
+            }
+            console.log(tmp)
+        }
+        console.log('------------   tbody   ---------------')
     }
 })()
