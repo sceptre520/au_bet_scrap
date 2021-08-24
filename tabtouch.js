f11bf08bbc05eee015a47ecd1e8e439e22455497
sceptre520 <denysshowdev@gmail.com>
2021-08-24 23:10:10 +0300

    tabt-1

--- tabtouch.js
+++ tabtouch.js
@@ -9,6 +9,7 @@ const getData = async () => {
     var mainDiv = $('#propResult')
     var tbody = $(mainDiv).children('table').children('tbody')
     var trs = $(tbody).children('tr')
+    console.log(trs)
     var tr_len = trs.length
     for(var tpm_i=0; tpm_i<tr_len; tpm_i+=3) {
         var matchname = $(trs[tpm_i]).children('td').children('strong').text()
