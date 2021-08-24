6e122b671c43676434abe25e2cbaffd1991dc6eb
sceptre520 <denysshowdev@gmail.com>
2021-08-25 00:45:39 +0300

    tabt-2

--- tabtouch.js
+++ tabtouch.js
@@ -1,24 +1,77 @@
 const cheerio = require('cheerio');
 const got = require('got');
 
+const vgmObjs= [
+    {
+        sport_key:'aussierules_afl',
+        sport_title:'AFL',
+        markets:[
+            {
+                key:'h2h',
+                url:'https://www.tabtouch.com.au/sports/australian-rules/afl/30929/1042715?tourname=Matches&tourseq=2800490&groupid=100&groupcode=--&groupname=Head-to-Head'
+            },
+            {
+                key:'spread',
+                url:'https://www.tabtouch.com.au/sports/australian-rules/afl/30929/1042715?tourname=Matches&tourseq=2800490&groupid=145&groupcode=--&groupname=Line'
+            }
+        ]
+    },
+    {
+        sport_key:'baseball_mlb',
+        sport_title:'MLB',
+        markets:[
+            {
+                key:'h2h',
+                url:'https://www.tabtouch.com.au/sports/baseball/mlb/30933/1042721?tourname=Matches&tourseq=2806702&groupid=110&groupcode=--&groupname=Match-Result'
+            },
+            {
+                key:'spread',
+                url:'https://www.tabtouch.com.au/sports/baseball/mlb/30933/1042721?tourname=Matches&tourseq=2806702&groupid=10000&groupname=Handicap-Betting'
+            },
+            {
+                key:'totals',
+                url:'https://www.tabtouch.com.au/sports/baseball/mlb/30933/1042721?tourname=Matches&tourseq=2806702&groupid=10000&groupname=Total-Runs'
+            }
+        ]
+    }
+]
 
-const getData = async () => {
-    const response = await got('https://www.tabtouch.com.au/sports/australian-rules/afl/30929/1042715?tourname=Matches&tourseq=2800490&groupid=100&groupcode=--&groupname=Head-to-Head');
-    const $ = cheerio.load(response.body);
+const getData = async (pmObj) => {
+    markets = {}
+    var mk_len = pmObj.markets.length
+    for(var mk_i=0; mk_i<mk_len; mk_i++) {
+        const response = await got(pmObj.markets[mk_i].url);
+        const $ = cheerio.load(response.body);
 
-    var mainDiv = $('#propResult')
-    var tbody = $(mainDiv).children('table').children('tbody')
-    var trs = $(tbody).children('tr')
-    var tr_len = trs.length
-    for(var tpm_i=0; tpm_i<tr_len; tpm_i+=3) {
-        var matchname = $(trs[tpm_i]).children('td').children('strong').text()
-        var team_1 = $(trs[tpm_i+1]).children('td').first().next().text()
-        var odd_1 = $(trs[tpm_i+1]).children('td').last().children('a').children('span').text()
-        var team_2 = $(trs[tpm_i+2]).children('td').first().next().text()
-        var odd_2 = $(trs[tpm_i+2]).children('td').last().children('a').children('span').text()
-        console.log(matchname)
-        console.log(team_1+' - '+odd_1+' : '+team_2+' - '+odd_2)
+        var mainDiv = $('#propResult')
+        var tbody = $(mainDiv).children('table').children('tbody')
+        var trs = $(tbody).children('tr')
+        var tr_len = trs.length
+        var tpm_i=0;
+        while(tpm_i<tr_len) {
+            var matchname = $(trs[tpm_i]).children('td').children('strong').text()
+            tmp_i ++;
+            var teams = []
+            var odds = []
+            while($(trs[tmp_i+1]).children('td').attr('colspan') == null) {
+                var team_name = $(trs[tpm_i]).children('td').first().next().text()
+                var odd_val = $(trs[tpm_i]).children('td').last().children('a').children('span').text()
+                teams.push(team_name)
+                odds.push(odd_val)
+                tmp_i ++
+            }
+            console.log(matchname)
+            console.log(teams)
+            console.log(odds)
+            // markets[team_1+'_vs_'+team_2].push({
+            //     key: pmObj.markets[mk_i].key,
+            //     outcomes: []
+            // })
+        }
     }
 }
 
-getData()
+var tmp_len = vgmObjs.length
+for(var tmp_i=0; tmp_i<tmp_len; tmp_i++) {
+    getData(vgmObjs[tmp_i])
+}
